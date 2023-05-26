09851ee49b123df3c21fd06c372ccc8995c75f6d
Ambis7 <95575174+Ambis7@users.noreply.github.com>
2023-05-26 05:16:10 +0300

    build

--- app/src/main/java/ui/components/AppDrawer.kt
+++ app/src/main/java/ui/components/AppDrawer.kt
@@ -1,175 +1,324 @@
-package ui.components
+package com.topic3.android.reddit.appdrawer
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.topic2.android.notes.routing.NotesRouter
-import com.topic2.android.notes.routing.Screen
-import com.topic2.android.notes.theme.NotesTheme
-import com.topic2.android.notes.theme.NotesThemeSettings
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import com.topic3.android.reddit.R
+import com.topic3.android.reddit.theme.RedditThemeSettings
 
+
+/**
+ * Представляет корневую композицию для панели приложений, используемой на экранах.
+ */
+@Composable
+fun AppDrawer(
+    modifier: Modifier = Modifier,
+    closeDrawerAction: () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colors.surface)
+    ) {
+        AppDrawerHeader()
+
+        AppDrawerBody(closeDrawerAction)
+
+        AppDrawerFooter(modifier)
+    }
+}
+
+/**
+ * Представляет заголовок drawer приложения со значком и названием приложения.
+ */
 @Composable
 private fun AppDrawerHeader() {
-    Row(modifier = Modifier.fillMaxWidth()) {
+    //TODO add your code here
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
         Image(
-            imageVector = Icons.Filled.Menu,
-            contentDescription = "Drawer Header Icon",
-            colorFilter = ColorFilter.
-            tint(MaterialTheme.colors.onSurface),
-            modifier = Modifier.padding(16.dp)
+            imageVector = Icons.Filled.AccountCircle,
+            colorFilter = ColorFilter.tint(Color.LightGray),
+            modifier = Modifier
+                .padding(16.dp)
+                .size(50.dp),
+            contentScale = ContentScale.Fit,
+            alignment = Alignment.Center,
+            contentDescription = stringResource(
+                id = R.string.account)
         )
         Text(
-            text = "Заметки",
-            modifier = Modifier
-                .align(alignment = Alignment.CenterVertically)
+            text = stringResource(R.string.default_username),
+            color = MaterialTheme.colors.primaryVariant
+        )
+        ProfileInfo()
+    }
+    Divider(
+        color = MaterialTheme.colors.onSurface.copy(alpha = .2f),
+        modifier = Modifier.padding(
+            start = 16.dp, end = 16.dp, top = 16.dp
+        )
+    )
+
+
+}
+
+@Composable
+fun ProfileInfo(modifier: Modifier = Modifier) {
+    //TODO add your code here
+    ConstraintLayout(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp)
+
+    ){
+        val (karmaItem, divider, ageItem) = createRefs()
+        val colors = MaterialTheme.colors
+
+        ProfileInfoItem(
+            Icons.Filled.Star,
+            R.string.default_karma_amount,
+            R.string.karma,
+            modifier = modifier.constrainAs(karmaItem){
+                centerVerticallyTo(parent)
+                start.linkTo(parent.start)
+            }
+        )
+
+        Divider(
+            modifier = modifier
+                .width(1.dp)
+                .constrainAs(divider) {
+                    centerVerticallyTo(karmaItem)
+                    centerHorizontallyTo(parent)
+                    height = Dimension.fillToConstraints
+                },
+            color = colors.onSurface.copy(alpha = .2f)
+        )
+        ProfileInfoItem(Icons.Filled.ShoppingCart,
+            R.string.default_reddit_age_amount,
+            R.string.reddit_age,
+            modifier = modifier.constrainAs(ageItem){
+                start.linkTo(divider.end)
+                centerVerticallyTo(parent)
+            }
+        )
+
+
+    }
+
+
+}
+
+@Composable
+private fun ProfileInfoItem(
+    iconAsset: ImageVector,
+    amountResourceId: Int,
+    textResourceId: Int,
+    modifier: Modifier
+) {
+    val colors = MaterialTheme.colors
+
+    ConstraintLayout(modifier=modifier) {
+        val (iconRef, amountRef, titleRef) = createRefs()
+        val itemModifier = Modifier
+        Icon(
+            contentDescription = stringResource(id = textResourceId),
+            imageVector = iconAsset,
+            tint = Color.Blue,
+            modifier = itemModifier
+                .constrainAs(iconRef) {
+                    centerVerticallyTo(parent)
+                    start.linkTo(parent.start)
+                }
+                .padding(start = 16.dp)
+        )
+        Text(
+            text = stringResource(amountResourceId),
+            color = colors.primaryVariant,
+            fontSize = 10.sp,
+            modifier = itemModifier
+                .padding(start = 8.dp)
+                .constrainAs(amountRef) {
+                    top.linkTo(iconRef.top)
+                    start.linkTo(iconRef.end)
+                    bottom.linkTo(titleRef.top)
+                }
+        )
+        Text(
+            text = stringResource(textResourceId),
+            color = Color.Gray,
+            fontSize = 10.sp,
+            modifier = itemModifier
+                .padding(start = 8.dp)
+                .constrainAs(titleRef) {
+                    top.linkTo(amountRef.bottom)
+                    start.linkTo(iconRef.end)
+                    bottom.linkTo(iconRef.bottom)
+                }
         )
     }
 }
 
-@Preview
+
+/**
+ * Представляет действия drawer приложения:
+ * * экранная навигация
+ * * светлый/темный режим приложения
+ */
 @Composable
-fun AppDrawerHeaderPreview(){
-    NotesTheme{
-        AppDrawerHeader()
+private fun AppDrawerBody(closeDrawerAction: () -> Unit) {
+    //TODO add your code here
+    Column{
+        ScreenNavigationButton(
+            icon = Icons.Filled.AccountBox ,
+            label = stringResource(R.string.my_profile),
+            onClickAction = { closeDrawerAction()
+            }
+        )
+        ScreenNavigationButton(
+            icon = Icons.Filled.Home,
+            label = stringResource(R.string.saved),
+            onClickAction = {
+                closeDrawerAction()
+            }
+        )
     }
 }
 
+/**
+ * Представляет компонент в панели приложений, который пользователь может использовать для смены экрана.
+ */
 @Composable
 private fun ScreenNavigationButton(
     icon: ImageVector,
     label: String,
-    isSelected: Boolean,
-    onClick:() -> Unit
+    onClickAction: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    val colors=MaterialTheme.colors
+    val colors = MaterialTheme.colors
 
-    val imageAlpha = if (isSelected){
-        1f
-    }else {
-        0.6f
-    }
-    val textColor = if (isSelected){
-        colors.primary
-    }else{
-        colors.onSurface.copy(alpha = 0.6f)
-    }
-    val backgroundColor = if (isSelected){
-        colors.primary.copy(alpha = 0.12f)
-    }else{
-        colors.surface
-    }
+    val surfaceModifier = modifier
+        .padding(start = 8.dp, top = 8.dp, end = 8.dp)
+        .fillMaxWidth()
+
     Surface(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 8.dp, end = 8.dp, top = 8.dp),
-        color = backgroundColor,
+        modifier = surfaceModifier,
+        color = colors.surface,
         shape = MaterialTheme.shapes.small
     ) {
-        Row(
-            horizontalArrangement = Arrangement.Start,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .clickable(onClick = onClick)
-                .fillMaxWidth()
-                .padding(4.dp)
-        ){
-            Image(imageVector = icon,
-                contentDescription = "Screen Navigation Button",
-                colorFilter = ColorFilter.tint(textColor),
-                alpha = imageAlpha
-            )
-            Spacer(Modifier.width(16.dp))
-            Text(
-                text = label,
-                style = MaterialTheme.typography.body2,
-                color = textColor,
-                modifier = Modifier.fillMaxWidth())
+        TextButton(
+            onClick = onClickAction,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Image(
+                    imageVector = icon,
+                    colorFilter = ColorFilter.tint(Color.Gray),
+                    contentDescription = label
+                )
+                Spacer(Modifier.width(16.dp))
+                Text(
+                    fontSize = 10.sp,
+                    text = label,
+                    style = MaterialTheme.typography.body2,
+                    color = colors.primaryVariant
+                )
+            }
         }
     }
 }
-@Preview
-@Composable
-fun ScreenNavigationButtonPreview(){
-    NotesTheme{
-        ScreenNavigationButton(icon = Icons.Filled.Home,
-            label = "Заметки",
-            isSelected = true,
-            onClick = {}
-        )
-    }
-}
 
+/**
+ * Представляет компонент настройки в панели приложений.
+ */
 @Composable
-private fun LightDarkThemeItem(){
-    Row(Modifier.padding(8.dp)
+private fun AppDrawerFooter(modifier: Modifier = Modifier) {
+    //TODO add your code here
+    ConstraintLayout(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(
+                start = 16.dp,
+                bottom = 16.dp,
+                end = 16.dp
+            )
+
     ) {
-        Text(text = "Включить тёмную тему",
+        val colors = MaterialTheme.colors
+        val (settingsImage, settingsText, darkModeButton) = createRefs()
+        Icon(
+            modifier = modifier.constrainAs(settingsImage){
+                start.linkTo(parent.start)
+                bottom.linkTo(parent.bottom)
+            },
+            imageVector = Icons.Default.Settings,
+            contentDescription = stringResource(R.string.settings
+            ),
+            tint = colors.primaryVariant
+
+        )
+        Text(
+            fontSize = 10.sp,
+            text = stringResource(R.string.settings),
             style = MaterialTheme.typography.body2,
-            color = MaterialTheme.colors.onSurface.copy(alpha = 0.6f),
-            modifier = Modifier
-                .weight(1f)
-                .padding(start = 8.dp, top = 8.dp, bottom = 8.dp)
-                .align(alignment = Alignment.CenterVertically)
+            color = colors.primaryVariant,
+            modifier = modifier
+                .padding(start = 16.dp)
+                .constrainAs(settingsText) {
+                    start.linkTo(settingsImage.end)
+                    centerVerticallyTo(settingsImage)
+                }
         )
-        Switch(
-            checked = NotesThemeSettings.isDarkThemeEnabled,
-            onCheckedChange ={ NotesThemeSettings.isDarkThemeEnabled = it},
-            modifier = Modifier
-                .padding(start = 8.dp, end = 8.dp)
-                .align(alignment = Alignment.CenterVertically)
+        Icon(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_moon),
+            contentDescription = stringResource(id = R.string.change_theme),
+            modifier = modifier
+                .clickable(onClick = { changeTheme() })
+                .constrainAs(darkModeButton) {
+                    end.linkTo(parent.end)
+                    bottom.linkTo(settingsImage.bottom)
+                },
+            tint = colors.primaryVariant
         )
     }
+}
+
+private fun changeTheme() {
+    RedditThemeSettings.isInDarkTheme.value = RedditThemeSettings.isInDarkTheme.value.not()
 }
 
 @Preview
 @Composable
-
-fun LightDarkThemeItemPreview(){
-    NotesTheme{
-        LightDarkThemeItem()
-    }
-}
-
-@Composable
-fun AppDrawer(
-    currentScreen: Screen,
-    closeDrawerAction: () -> Unit
-){
-    Column(modifier = Modifier.fillMaxSize()) {
-        AppDrawerHeader()
-        Divider(color = MaterialTheme.colors.onSurface.copy(alpha = .2f))
-
-        ScreenNavigationButton(icon = Icons.Filled.Home, label ="Заметки" , isSelected = currentScreen == Screen.Notes,
-            onClick = {
-                NotesRouter.navigateTo(Screen.Notes)
-                closeDrawerAction()
-            }
-        )
-        ScreenNavigationButton(icon = Icons.Filled.Delete, label ="Корзина" , isSelected = currentScreen == Screen.Trash,
-            onClick = {NotesRouter.navigateTo(Screen.Trash)
-                closeDrawerAction()
-
-            }
-        )
-        LightDarkThemeItem()
-    }
-}
-@Preview
-@Composable
-fun AppDrawerPreview(){
-    NotesTheme{
-        AppDrawer(Screen.Notes) {}
-    }
+private fun ProfileInfoItemPreview() {
+    ProfileInfoItem(
+        Icons.Filled.ShoppingCart,
+        R.string.default_reddit_age_amount,
+        R.string.reddit_age,
+        Modifier
+    )
 }
